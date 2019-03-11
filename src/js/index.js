8fce0ac0f71a00193afe211e1e2478eef83463eb
Harsh <akshatrana2007@gmail.com>
2019-03-10 21:50:04 -0400

    imported api key into file

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
 // Global app controller
+import axios from 'axios';
 
-
+async function getresults(query){
+   const proxy = 'https://cors-anywhere.herokuapp.com/';
+   const key = 'd5582bf9629e11a649819a5b4d084cd1';
+   try {
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+   } catch(error) {
+       alert(error);
+   }
+   
+}
+getresults('pizza');
+//https://www.food2fork.com/api/search
+//d5582bf9629e11a649819a5b4d084cd1 
 
