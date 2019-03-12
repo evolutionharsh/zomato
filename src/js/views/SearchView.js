ed2b048837d81a12db93244ce1596b773fd1fa83
Harsh <akshatrana2007@gmail.com>
2019-03-12 15:42:45 -0400

    created button to implement pagination forward & backward

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -8,6 +8,7 @@ elements.searchInput.value = '';
 
 export const clearResults = () => {
     elements.searchResList.innerHTML ='';
+    elements.searchResPages.innerHTML = '';
 };
 
 /*
@@ -55,8 +56,43 @@ const renderRecipe =  recipe => {
     `;
     elements.searchResList.insertAdjacentHTML('beforeend',markup);
 };
+//type: 'prev' or 'next'
+const createButton = (page,type) => `
+<button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+<span>Page ${type === 'prev' ? page -1 : page + 1}</span>
+    <svg class="search__icon">
+        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+    </svg>
+   
+</button>
+`;
 
-export const renderResults = recipes => {
-    //console.log(recipes);
-    recipes.forEach(renderRecipe);
+const renderButtons =(page, numResults, resPerPage)=>{
+   
+  const pages = Math.ceil(numResults/resPerPage);
+  let button;
+  if(page === 1 && pages > 1 )
+  {
+     //Only Button to go to the next page
+      button = createButton(page,'next');
+  } else if (page === pages && pages > 1) {
+     //Button to go to the prev page
+     button = createButton(page,'prev');
+  } else if (page < pages) {
+    //Both Buttons
+    button = `
+     ${createButton(page,'prev')}
+     ${createButton(page,'next')};
+     `;
+ } 
+
+ elements.searchResPages.insertAdjacentHTML('afterbegin',button);
+};
+export const renderResults = (recipes,page = 1,resPerPage = 10) => {
+    //render results of the current page
+    const start = (page -1)* resPerPage;
+    const end = page * resPerPage;
+    recipes.slice(start,end).forEach(renderRecipe);
+    //render pagination buttons 
+    renderButtons(page,recipes.length,resPerPage);
 };
