4f69eeb5b1922fe4bec2fe78aacdf05f393ee9b2
Harsh <akshatrana2007@gmail.com>
2019-03-21 23:37:18 -0400

    added highlightedselected constant

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -11,6 +11,13 @@ export const clearResults = () => {
     elements.searchResPages.innerHTML = '';
 };
 
+export const highlightSelected = id => {
+  const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+  resultsArr.forEach(el => {
+   el.classList.remove('results__link--active');
+  });
+  document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+};
 /*
 // 'Pasta with tomato and spinach'
 acc: 0 / acc + cur.length = 5 / newTitle =['Pasta']
@@ -18,7 +25,7 @@ acc: 5 / acc + cur.length = 9 / newTitle =['Pasta,'with']
 acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta','with','tomato']
 acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta','with','tomato']
 acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta','with','tomato']
-
+)
 
 */
 
